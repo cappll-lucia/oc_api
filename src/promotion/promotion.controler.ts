04ec3c825a51fe977f00d70d326e20e343c08231
lucia-cappll <luciacappll@gmail.com>
2024-02-22 14:37:26 -0300

    fixed em error

--- src/promotion/promotion.controler.ts
+++ src/promotion/promotion.controler.ts
@@ -178,7 +178,6 @@ export async function update(req: Request, res: Response) {
 export async function remove(req: Request, res: Response) {
   try {
     const id = Number.parseInt(req.params.id);
-    const em = orm.em.fork();
     const promotion = await em.findOneOrFail(Promotion, id);
     await em.removeAndFlush(promotion);
     res
@@ -195,6 +194,7 @@ export async function remove(req: Request, res: Response) {
 export const uploadPromoBannerMiddleware = upload.single('banner');
 export async function uploadPromoBanner(req: Request, res: Response) {
   try {
+    const em = orm.em.fork();
     const id = Number.parseInt(req.params.id);
     const promotion = await em.findOneOrFail(Promotion, id);
     const banners = JSON.parse(promotion.banner_url);
